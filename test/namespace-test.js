d8a2d0eb14ed87451927dde099f128c68145cf6e
Bruno Sanches <brunopsanches@gmail.com>
2015-07-19 13:52:09 -0700

    Namespaces contain `hooks`

--- test/namespace-test.js
+++ test/namespace-test.js
@@ -6,9 +6,14 @@ var Namespace = require('../lib/namespace');
 
 describe('Namespace', function () {
   describe('constructor', function () {
-
     it('returns a Function constructor', function () {
       assert.equal(typeof Namespace, 'function');
+    });
+
+    it('returns an object with a `hooks` object property', function () {
+      var namespace = new Namespace();
+
+      assert.equal(typeof namespace.hooks, 'object');
     });
   });
 });
